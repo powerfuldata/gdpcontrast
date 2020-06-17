1f3861b0fec2af93b4a05404b87d457722bec143
shiyuan2he <shiyuan4work@126.com>
2020-06-17 20:08:17 +0800

    完善开发

--- data-server/src/main/java/org/gdp/contrast/server/core/resolver/impl/GdpPageResolver.java
+++ data-server/src/main/java/org/gdp/contrast/server/core/resolver/impl/GdpPageResolver.java
@@ -8,7 +8,9 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -24,23 +26,26 @@ public class GdpPageResolver implements PageResolver {
         log.info("【解析器】开始解析页面");
         Document htmlDoc = page.getDocument();
         try {
+            List<Map<String, String>> dataList = new ArrayList<>();
             Element table = htmlDoc.getElementById("table_id");
             Elements elements = table.getElementsByTag("tbody");
             elements.forEach(data -> {
                 data.getElementsByTag("tr").forEach(
                     trEle -> {
-                        log.info("{},{},{},{}",
-                                trEle.getElementsByIndexEquals(0).text(),
-                                trEle.getElementsByIndexEquals(1).text(),
-                                trEle.getElementsByIndexEquals(2).text(),
-                                trEle.getElementsByIndexEquals(3).text()
-                        );
+                        Map<String, String> dataMap = new HashMap<>();
+                        Elements tdArray = trEle.getElementsByTag("td");
+                        dataMap.put("rank", tdArray.get(0).text());
+                        dataMap.put("country", tdArray.get(1).text());
+                        dataMap.put("2018", tdArray.get(2).text());
+                        dataMap.put("2019", tdArray.get(3).text());
+                        dataList.add(dataMap);
                     }
                 );
             });
             //用来存放爬取到的信息，供之后存储！map类型的即可，可以自定义各种嵌套！
-            Map<String, String> items = new HashMap<>();
+            Map<String, Object> items = new HashMap<>();
             //放入items中，之后会自动保存（如果你自己实现了下载器，请自己操作它。如下我自定义了自己的下载器，并将它保存到了文本中！）！
+            items.put("data", dataList);
             page.setItems(items);
         } catch (NullPointerException e) {
             log.info("该页面没有解析到相关东西！跳过");
--- data-server/src/main/java/org/gdp/contrast/server/core/storager/impl/ConsoleSaver.java
+++ data-server/src/main/java/org/gdp/contrast/server/core/storager/impl/ConsoleSaver.java
@@ -1,4 +1,5 @@
 package org.gdp.contrast.server.core.storager.impl;
+import com.alibaba.fastjson.JSONArray;
 import lombok.extern.slf4j.Slf4j;
 import org.gdp.contrast.server.core.model.Page;
 import org.gdp.contrast.server.core.storager.Saver;
@@ -8,19 +9,17 @@ import java.util.Map;
 /**
  * @description <p>控制台存储器，将爬虫内容输出在控制台上，此时logback.xml日志输出级别设置成error</p>
  * @author heshiyuan
- * @date 22/09/2017 2:26 PM
- * @email shiyuan4work@sina.com
- * @github https://github.com/shiyuan2he.git
- * Copyright (c) 2016 shiyuan4work@sina.com All rights reserved
  */
 @Slf4j
 public class ConsoleSaver implements Saver {
 
     @Override
     public Page save(Page page) {
-        log.info("【保存器】");
+        log.info("【保存器】...");
         Map<Object, Object> map = page.getItems();
-        map.forEach((k, v) -> System.out.println(k + " : " + v));
+        map.forEach((k, v) -> {
+            log.info("【保存器】{}", JSONArray.toJSONString(v));
+        });
         return page;
     }
 
